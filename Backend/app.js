abd340f334e604addfd579e5d6222e85433102bd
Muertinos <martinm9896@gmail.com>
2019-11-06 22:56:28 +0100

    Routes and models
    Added routes and models from tutorial

--- Backend/app.js
+++ Backend/app.js
@@ -1,9 +1,60 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
+const productRoutes = require('./api/routes/products');
+const orderRoutes = require('./api/routes/orders');
+
+const { mongodbPassword } = require('./config');
+
+//Logging
+app.use(morgan('dev'));
+
+//BodyParser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+//Append headers to any routes (Placed before the routes, fixes CORS errors)
 app.use((req, res, next) => {
-    res.status(200).json({
-        message: 'It works'
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    if(req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+
+        //return is used because of additional responses, empty object because no routes are used - just the response code
+        return res.status(200).json({});
+    }
+    next();
+});
+
+//Routes
+app.use('/products', productRoutes);
+app.use('/orders', orderRoutes);
+
+//Database connection
+mongoose.connect('mongodb+srv://pwe0918_admin:' + mongodbPassword + '@pwe0918-bachelor-anp5j.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+//Incase none of the routes could handle the incoming request
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error); //forwards the error request
+});
+
+//Handles errors thrown from anywhere in the application
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
     });
 });
 
--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,11 @@
 const http = require('http');
 const app = require('./app');
+const { port } = require('./config');
 
-const port = process.env.PORT || 3000;
+
+const serverPort = port || 3000;
+console.log(`Node.js is running on port ${serverPort}`);
 
 const server = http.createServer(app);
 
-server.listen(port);
+server.listen(serverPort);
