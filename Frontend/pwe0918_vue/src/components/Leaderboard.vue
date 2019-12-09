08493f35e4948ee86c8afff29df78f16bcfdf301
Muertinos <martinm9896@gmail.com>
2019-12-09 13:00:00 +0100

    Leaderboard
    Added functionality for Leaderboard (calc by steps, trash & m2)

--- Frontend/pwe0918_vue/src/components/Leaderboard.vue
+++ Frontend/pwe0918_vue/src/components/Leaderboard.vue
@@ -12,7 +12,7 @@
                 </v-btn>
               </template>
               <v-list>
-                <v-list-item v-for="(filter, index) in filters" :key="index" @click="activeFilter = filter;">
+                <v-list-item v-for="(filter, index) in filters" :key="index" @click="activeFilter = filter; changeSort();">
                   <v-list-item-title>{{ filter }}</v-list-item-title>
                 </v-list-item>
               </v-list>
@@ -30,19 +30,25 @@
           <v-row>
             <v-col cols="12">
               <v-sheet v-show="currentTab === 'tab-uge'">
+                <v-btn @click="changeSort">change sort</v-btn> current sort: {{ defaultSort }}
                 <v-list>
                   <v-list-item-group>
                     <v-list-item color="success" disabled> <!--Header-->
                       <v-container fluid class="py-0">
                         <v-row>
-                          <v-col cols="1" class="py-0">
+                          <v-col cols="2" class="py-0">
                             <v-list-item-content>
                               <v-list-item-title>Rank</v-list-item-title>
                             </v-list-item-content>
                           </v-col>
-                          <v-col cols="8" class="py-0">
+                          <v-col cols="3" class="py-0">
                             <v-list-item-content>
-                              <v-list-item-title>Navn</v-list-item-title>
+                              <v-list-item-title>Antal m2</v-list-item-title>
+                            </v-list-item-content>
+                          </v-col>
+                          <v-col cols="4" class="py-0">
+                            <v-list-item-content>
+                              <v-list-item-title>Skridt</v-list-item-title>
                             </v-list-item-content>
                           </v-col>
                           <v-col cols="3" class="py-0" align="end">
@@ -53,22 +59,27 @@
                         </v-row>
                       </v-container>
                     </v-list-item>
-                    <v-list-item v-for="(user, index) in allUsers" :key="index"> <!--All users-->
+                    <v-list-item v-for="(user, index) in todos" :key="user._id">
                       <v-container fluid class="py-0">
                         <v-row>
-                          <v-col cols="1" class="py-0">
+                          <v-col cols="2" class="py-0">
                             <v-list-item-content>
-                              <v-list-item-title></v-list-item-title>
+                              <v-list-item-title>{{ index+1 }}</v-list-item-title>
                             </v-list-item-content>
                           </v-col>
-                          <v-col cols="8" class="py-0">
+                          <v-col cols="3" class="py-0">
                             <v-list-item-content>
-                              <v-list-item-title>{{ user.username }}</v-list-item-title>
+                              <v-list-item-title>{{ user.userData.totalSqMeters }}</v-list-item-title>
+                            </v-list-item-content>
+                          </v-col>
+                          <v-col cols="4" class="py-0">
+                            <v-list-item-content>
+                              <v-list-item-title>{{ user.userData.totalSteps }}</v-list-item-title>
                             </v-list-item-content>
                           </v-col>
                           <v-col cols="3" class="py-0" align="end">
                             <v-list-item-content>
-                              <v-list-item-title></v-list-item-title>
+                              <v-list-item-title>{{ user.userData.totalTrashInGram }}</v-list-item-title>
                             </v-list-item-content>
                           </v-col>
                         </v-row>
@@ -103,8 +114,20 @@ import axios from 'axios';
         tabs: [ 'uge', 'måned', 'altid' ],
         currentTab: 'tab-uge',
 
+        sortBySteps: true,
+        sortByTrash: false,
+        defaultSort: 'steps',
+        steps: 0,
+        trash: 0,
+        sqMeters: 0,
+
         baseUrl: 'http://localhost:8626/',
-        allUsers: []
+        allUsers: [],
+      }
+    },
+    watch: {
+      activeFilter: function() {
+        this.changeSort();
       }
     },
     methods: {
@@ -123,9 +146,63 @@ import axios from 'axios';
           console.log(error);
         });
       },
-      // sortBySteps() {
+      changeSort() {
+        if (this.activeFilter == 'Skridt') {
+          this.sortBySteps = true;
+          this.sortByTrash = false;
+        } else if (this.activeFilter == 'Affald') {
+          this.sortByTrash = true;
+          this.sortBySteps = false;
+        } else {
+          this.sortBySteps = false;
+          this.sortByTrash = false;
+        }
+        // this.sortBySteps = !this.sortBySteps;
+        // if (this.sortBySteps) {
+        //   this.sort = 'steps';
+        // } else if (this.sortByTrash) {
+        //   this.sort = 'trash';
+        // } else {
+        //   this.sort = 'sqMeters';
+        // }
+      }
+    },
+    computed: {
+      todos() {
+        let result = this.allUsers;
+        // let result = [
+        //   { id:1, text: "Learn JavaScript", show: true, price: 100, duration: 2 },
+        //   { id:2, text: "Learn Vue", show: true, price: 200, duration: 1 },
+        //   { id:3, text: "Play around in JSFiddle", show: true, price: 500, duration: 5 },
+        //   { id:4, text: "Build something awesome", show: true, price: 550, duration: 4 }
+        // ];
+        // result = result.filter(todo => todo.price >= this.price);
+        result = result.filter(user => user.userData.totalSteps >= this.steps);
 
-      // }
+        // if(this.sortByPrice) {
+        //   return result.sort(function(a, b) {
+        //     return b.price - a.price;
+        //   });
+        // } else {
+        //   return result.sort(function(a, b) {
+        //     return a.duration - b.duration;
+        //   });
+        // }
+
+        if(this.sortBySteps) {
+          return result.sort(function(a,b) {
+            return b.userData.totalSteps - a.userData.totalSteps;
+          });
+        } else if(this.sortByTrash) {
+          return result.sort(function(a,b) {
+            return b.userData.totalTrashInGram - a.userData.totalTrashInGram;
+          });
+        } else {
+          return result.sort(function(a,b) {
+            return b.userData.totalSqMeters - a.userData.totalSqMeters;
+          });
+        }
+      }
     },
     async mounted() {
       await this.getAllUsers();
